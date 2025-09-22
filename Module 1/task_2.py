7c4baf7a914d76ff9f2b62f85e7aac42e122385f
awal <awal.aaraf.92@gmail.com>
2025-09-22 11:00:59 +0600

    Final Commit

--- Module 1/task_2.py
+++
@@ -1,3 +0,0 @@
-lst=["python", "java", "c++", "javascript"]
-
-print(f"{'Python found' if 'python' in lst else 'Not found' }")
--- Module 1/task_3.py
+++
@@ -1,9 +0,0 @@
-files = ["report.pdf", "image.png", "notes.txt", "slides.pdf"]
-
-for st in files:
-    print(f"{'PDF file found' if st.endswith('.pdf') else 'Other file'}")
-
-# i=0
-# while i<len(files):
-#     print(f"{'PDF file found' if files[i].endswith('.pdf') else 'Other file'}")
-#     i+=1
