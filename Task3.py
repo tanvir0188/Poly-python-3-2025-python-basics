cc4f1b6d8667fc47253553844cc4a0665450966e
zannatul magferati <magferatizannatul@gmail.com>
2025-09-23 21:04:26 +0600

    Update Task3.py

--- Task3.py
+++ Task3.py
@@ -39,3 +39,41 @@ except IndexError:
     print("Error: Index is not available!")
 
 
+"""
+Task 2 of the function class 'another way,
+"""
+
+def process_numbers(numbers, operation):
+  num = []
+  if operation == "double":
+    for number in numbers:
+      num.append(number*2)
+    return num
+
+  elif operation == "square":
+    for number in numbers:
+      num.append(number**2)
+    return num
+
+  elif operation == "cube":
+    for number in numbers:
+      num.append(number**3)
+    return num
+
+  elif operation == "filter_even":
+    for number in numbers:
+      if number %2 ==0:
+        num.append(number)
+    return num
+
+  elif operation == "sum":
+    sum = 0
+    for number in numbers:
+      sum = sum+number
+    return sum
+
+  else:
+    return "Invalid operation!"
+
+
+
