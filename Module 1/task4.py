6aa0305db7f583a50302035cf793f27d6bb22a6e
habiba bushra <shifakhadija246@gmail.com>
2025-09-24 03:00:51 +0600

    updated

--- Module 1/task4.py
+++ Module 1/task4.py
@@ -1,20 +1,23 @@
-#task1
+#Task 1:
 #Create a Family object. It should have attribute such as my_name, father_name, mother_name, sister_name, brother_name
 #You have to create the class using a constructor. Initiate it and set values to the attributes.
 
 
 class Family:
-    def __int__ (self, fathername, mothername, sistername, brothernam, myname):
-        self.father_name=fathername
-        self.mother_name=mothername
-        self.sister_name=sistername
-        self.brother_name=brothername
-        self.my_name=myname
-family=Family('matin','ripa','maisha', 'raiyan','shifa')
-print(f'father name: {Family.father_name}, mother name: {Family.mother_name}, sister name: {Family.sister_name}, brother name: {Family.brother_name}, my name: {Family.my_name}')
+
+  def __int__(self, my_name, father_name, mother_name, sister_name, brother_name):
+      self.my_name=my_name
+      self.father_name=father_name
+      self.mother_name=mother_name
+      self.sister_name=sister_name
+      self.brother_name=brother_name
+
+family = Family('khadija', 'matin', 'ripa', 'maisha', 'raiyan')
+print(f'family_name:{my.my_name}, family_name:{father.father_name}, family_name:{mother.mother_name}, family_name:{sister.sister_name}, family_name:{brother.brother_name}')
 
 
-#task2
+
+#Task 2:
 #Create a rectangle, a triangle and a square object.
 #Initialize them. Find out the area and perimeter of each object.
 
@@ -25,45 +28,33 @@ print(f'father name: {Family.father_name}, mother name: {Family.mother_name}, si
 
 #NB: If you can use class method then do it. Otherwise you can use constructor only
 
-
-
 class Rectangle:
-     def __int__ (self, height, width):
-        self.height = height
-        self.width = width
-
-    def area(self):
-        return self.height * self.width
-
-    def perimeter(self):
-        return 2 * (self.height + self.width)
-
-rectangle=Rectangle(4,5)   
-print(f'regtangle length: {rectangle.length}, rectangle width: {rectangle.width}, rectangle area: {rectangle.area()}, rectangle perimeter: {rectangle.perimeter}')
-
-
-        
+   def __init__(self, width, height):
+      self.width= width
+      self.height= height
+       
+   def area(self):
+        return self.width*self.height
+   def perimeter(self):
+        return 2*(self.width+self.height)
+rectangle= Rectangle(2,3)
+print(f'rectangle width: {rectangle.width}, rectangle height: {rectangle.height}, rectangle area: {rectangle.area()}, rectangle perimeter: {rectangle.perimeter()}')
+      
 class Square:
     def __init__(self, side):
-        self.side = side   
-    
+        self.side=side
     def area(self):
-        return self.side * self.side
-    
+        return self.side*self.side
     def perimeter(self):
-        return 4 * self.side
-
-square = Square(4)
-print(f'square side:{square.side}, square area:{square.area()}, square perimeter:{square.perimeter()}')
-
+        return 4*self.side
+square= Square(4)
+print(f'square side: {square.side}, square area: {square.area()}, square perimeter: {square.perimeter()}')
 
 
 class Triangle:
-    def __int__(self,base,height):
-       self.base= base
-       self.height= height
+    def __init__(self, base, height):
+        self.base = base
+        self.height = height
 
     def area(self):
         return 0.5 * self.base * self.height
-    
-    def perimeter(self):
