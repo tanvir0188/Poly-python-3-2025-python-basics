ae44d3dc1b21ea3b62abfa1849d7f034ea630c8c
yousufmiah619 <yousufmiah619@gmail.com>
2025-09-23 11:42:51 +0600

    oop practic and task

---
+++ Tutorial 2/oop_task.py
@@ -0,0 +1,41 @@
+# some animal:
+class Dog():
+    species="canis familiaris"               #class attribute
+    def __init__(self,name,age,color):
+        self.dog_name=name
+        self.dog_age=age
+        self.dog_color=color
+        
+dog1=Dog("tomy",5,"black")
+dog2=Dog("tom",5,"red")
+print(f'I have a {dog1.species} dog.my dog name {dog1.dog_name} . this is  {dog1.dog_age} years old.this dog {dog1.dog_color} color is very nice.')
+
+print(f'I have a {dog2.species} dog.my dog name {dog2.dog_name} . this is  {dog2.dog_age} years old.this dog {dog2.dog_color} color is very nice.')
+
+class Cat():
+    spacies="Falis cats"
+    def __init__(self,name,age,color):
+        self.cat_name=name
+        self.cat_age=age
+        self.cat_color=color
+cat1=Cat("jany",1,"white")
+
+print(f"I have a {cat1.spacies} cat .my cat namse is : {cat1.cat_name} . ihis is {cat1.cat_age} yeras old.there {cat1.cat_color} color is very buitifull.")
+
+
+
+
+#Task 1:
+#Create a Family object. It should have attribute such as my_name, father_name, mother_name, sister_name, brother_name
+#You have to create the class using a constructor. Initiate it and set values to the attribu 
+
+class Family():
+    def __init__(self,my_name,father_name,mother_name,brother_name,sister_name):
+        self.my_name=my_name
+        self.father_name=father_name
+        self.mother_name=mother_name
+        self.brother_name=brother_name
+        self.sister_name=sister_name
+        
+f1=Family("Yousuf miah","Tukku miah","Rehena begum","Ibrahim miah","asma akter")
+print(f"my name is : {f1.my_name} . my father name is : {f1.father_name} . my mother name is : {f1.mother_name} .my brother name is  : {f1.brother_name} . my sister name is {f1.sister_name} ." )
