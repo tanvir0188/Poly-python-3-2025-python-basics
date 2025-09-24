3775a47097eb1bd33b5485dbfeee51b3ef511552
jihad rana <jiharana1971@gmail.com>
2025-09-24 10:24:04 +0600

    new task

--- Tutorial 3/task_1.py
+++
@@ -1,8 +0,0 @@
-numb_1 = [[2, 4, 1, 5], [2, 10, 12], [14, 16, 20]]
-
-for sub_list in numb_1:       
-    for n in sub_list:    
-        if n % 2 == 0:
-            print(n, "is an even number.")
-        else:
-            print(n, "is an odd number.")
---
+++ Tutorial/task_1.py
@@ -0,0 +1,17 @@
+class Family:
+    
+    
+    def __init__(self, my_name, father_name, mother_name, sister_name, ):
+        self.my_name = my_name
+        self.father_name = father_name
+        self.mother_name = mother_name
+        self.sister_name = sister_name
+       
+
+my_family = Family("Jihad", "babul hossen", "rawshanara Begum", "bonna", )
+
+
+print("My Name:", my_family.my_name)
+print("Father's Name:", my_family.father_name)
+print("Mother's Name:", my_family.mother_name)
+print("Sister's Name:", my_family.sister_name)
