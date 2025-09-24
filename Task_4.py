e7fc0b0c4f38390cf11310175f8bd46e1f4736d6
Mst. Aka Akther <akaakther188@gmail.com>
2025-09-24 11:29:23 +0600

    Add task

--- Task_4.py
+++ Task_4.py
@@ -11,6 +11,7 @@ member1= family("asd","bnds","ksjd","djds","jjd")
 print(f"family_member:{member1.father_name},{member1.mother_name},{member1.sister_name},{member1.brother_name},{member1.my_name}")
 
 #Task-4:
+import math
 class Rectangle:
     def __init__(self,width,height):
         self.width = width
@@ -19,6 +20,33 @@ class Rectangle:
         return self.width*self.height
     def perimeter(self):
         return 2 * (self.width + self.height)
+
+class Triangle:
+    def __init__(self,base,height):
+        self.base = base
+        self.height = height
+    def area(self):
+        return 0.5 * self.base * self.height
+    def perimeter(self):
+        hypo = math.sqrt(self.base**2 + self.height**2)
+        return self.base + self.height + hypo
+
+class Square:
+    def __init__(self,side):
+        self.side = side
+    def area(self):
+        return self.side * self.side
+    def perimeter(self):
+        return 4 * self.side
+
 rectangle = Rectangle(2,4)
 print("Rectangle Area:",rectangle.area())
 print("Rectangle Perimeter:",rectangle.perimeter())
+
+triangle = Triangle(5,6)
+print("Triangle Area:", triangle.area())
+print("Triangle Perimeter:", triangle.perimeter())
+
+square = Square(4)
+print("Square Area:",square.area())
+print("Square Perimeter:",triangle.perimeter())
